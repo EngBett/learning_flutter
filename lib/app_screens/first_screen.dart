c3caac580aec50d3d9b10271124758ca5f7da356
Bett <bettthetimetraveller@gmail.com>
2019-08-30 17:17:07 +0300

    first Screen

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,8 +1,7 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-class FirstScreen extends StatelessWidget{
-
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -10,20 +9,18 @@ class FirstScreen extends StatelessWidget{
         color: Colors.white,
         child: Center(
             child: Text(
-                "Your Number is: ${generateNumber()}",
+                //"Your Number is: ${generateNumber()}",
+                generateNumber(),
                 textDirection: TextDirection.ltr,
-                style: TextStyle(
-                    color: Colors.lightBlue,
-                    fontSize: 18.0
-                )//style
-            ) //Text
-        ) //Center
-    ); //Material
+                style:
+                    TextStyle(color: Colors.lightBlue, fontSize: 18.0) //style
+                ) //Text
+            ) //Center
+        ); //Material
   }
 
-  int generateNumber(){
+  String generateNumber() {
     var rand = Random();
-    return rand.nextInt(10);
+    return "Your Number is: ${rand.nextInt(10)}";
   }
-
 }
